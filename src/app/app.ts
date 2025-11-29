4207099772c2a3f8222bb3377f95dd01f0bc772a
Grzegorz Major <major.grzegorz@gmail.com>
2025-11-29 17:49:03 +0100

    Set up basic components structure with global styles and updated Angular CLI configuration.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
+import {TodoList} from './todo-list/todo-list';
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    TodoList,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
